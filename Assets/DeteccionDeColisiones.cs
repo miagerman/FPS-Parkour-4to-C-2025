716b6f0d66849a27cd8e54b188b2a7f262386c8b
miagerman <49194476@est.ort.edu.ar>
2025-05-29 11:36:21 -0300

    Como hacer una colisión

---
+++ Assets/DeteccionDeColisiones.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(collision.gameObject);
+    }
+
+}
